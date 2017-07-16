4828524fc9419ab0a45e223306f49b724ee44758
SunnyMarkLiu <SunnyMarkLiu101@gmail.com>
2017-07-16 20:38:05 +0800

    clean code

--- model/vgg16_fine_tuning.py
+++ model/vgg16_fine_tuning.py
@@ -12,7 +12,7 @@ module_path = os.path.abspath(os.path.join('..'))
 sys.path.append(module_path)
 
 from keras.models import Model
-from keras.layers import Dropout, Flatten, Dense
+from keras.layers import Input, Dropout, Flatten, Dense
 from keras import optimizers
 from keras import applications
 from keras import backend as K
@@ -24,35 +24,34 @@ import keras
 def main():
     data_wapper = data_util.DataWapper(image_size=224)
 
-    if K.image_data_format() == 'channels_first':
+    if K.image_data_format() == 'channels_first':  # theano
         input_shape = (3, 224, 224)
-    else:
+    else:  # tensorflow
         input_shape = (224, 224, 3)
 
     print 'built vgg16 model'
     # build the VGG16 network
-    model = applications.VGG16(weights='imagenet', include_top=False, input_shape=input_shape)
+    image_input = Input(shape=input_shape)
+    model = applications.VGG16(weights='imagenet', include_top=False, input_tensor=image_input)
     print('Model loaded.')
+    for layer in model.layers:
+        print 'frozen layer', layer
+        layer.trainable = False
 
     # build a classifier model to put on top of the convolutional model
-    top_model = Flatten(name='flatten', input_shape=model.output_shape[1:])(model.output)
+    top_model = Flatten(name='flatten')(model.output)
     top_model = Dense(256, activation='relu', name='fc1')(top_model)
     top_model = Dropout(0.5)(top_model)
     top_model = Dense(256, activation='relu', name='fc2')(top_model)
     top_model = Dropout(0.5)(top_model)
     top_model = Dense(1, activation='softmax', name='predictions')(top_model)
 
-    model = Model(model.input, top_model, name='vgg16')
-    # set the first 19 layers (up to the last conv block)
-    # to non-trainable (weights will not be updated)
-    for layer in model.layers[:19]:
-        print 'frozen layer', layer
-        layer.trainable = False
+    model = Model(input=image_input, output=top_model, name='vgg16')
 
     # compile the model with a SGD/momentum optimizer
     # and a very slow learning rate.
     model.compile(loss='binary_crossentropy',
-                  optimizer=optimizers.SGD(lr=1e-4, momentum=0.9),
+                  optimizer=optimizers.Adagrad(lr=0.03, epsilon=1e-06),
                   metrics=['accuracy'])
     print(model.summary())
     plot_model(model, to_file='vgg16_model.png')
